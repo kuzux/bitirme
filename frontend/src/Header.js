bb9e88589935ee0cfc75efba32c1a02f08a6d589
Arda Cinar <arda@cinar.be>
2017-02-09 19:39:32 +0300

    Navbar

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 import { connect } from 'react-redux';
+import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
 const Header = (props) => {
-    return <div></div>;
+    return (
+        <Navbar>
+            <Navbar.Header>
+                <Navbar.Brand>
+                   <a href="#">A Multivariate Data Visualization Thing</a>
+                </Navbar.Brand>
+            </Navbar.Header>
+            <Nav>
+                <NavItem eventKey={1} href="/">Home</NavItem>
+            </Nav>
+        </Navbar>
+    );
 };
 
 const mapStateToProps = (state: Map<any, any>): {} => {
