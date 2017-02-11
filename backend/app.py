7bbfe3b912ddb587d66ddabf37da623aac5abe5c
Arda Cinar <arda@cinar.be>
2017-02-11 13:21:10 +0300

    Added dummy backend calls

--- backend/app.py
+++ backend/app.py
@@ -6,4 +6,21 @@ app = Flask(__name__)
 def hello():
     return "Hello 492"
 
+@app.route('/group-by/<interval>/<operation>')
+def group_by(interval, operation):
+    intervals = ['hour', 'day', 'week', 'month']
+    ops = ['sum', 'product', 'min', 'max']
+    
+    if not interval in intervals:
+        return "Invalid time interval"
+
+    if not operation in ops:
+        return "Invalid operation"
+
+    return "Group by"
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    return "No upload yet"
+
 app.run(debug=True)
